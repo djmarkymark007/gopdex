4487cb9c2ac256ac36ecb9ab09abd9373603a357
Mark Henmi <mrhenmi@gmail.com>
2024-04-21 22:04:25 -0700

    add some basic cli commands

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/djmarkymark007/gopdex
+
+go 1.22.2
--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println(`Welcome to the Pokedex!
+Usage:
+	
+help: Displays a help message
+exit: Exit the Pokedex`)
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
 func main() {
-	fmt.Println("hello world")
+	commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Display's help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "closes the program",
+			callback:    commandExit,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+		val, ok := commands[scanner.Text()]
+		if !ok {
+			fmt.Println("Invaild input. \ncall help for more info")
+			continue
+		}
+		val.callback()
+	}
 }
